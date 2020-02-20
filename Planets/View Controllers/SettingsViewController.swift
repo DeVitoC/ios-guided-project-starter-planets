021490948a16ef7716781b3d71b5d98a24b8c856
Christopher DeVito <toolofjesus@protonmail.com>
2020-02-19 22:29:47 -0500

    UI complete

---
+++ Planets/View Controllers/SettingsViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SettingsViewController.swift
+//  Planets
+//
+//  Created by Lambda_School_Loaner_259 on 2/19/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    @IBOutlet var plutoToggle: UISwitch!
+    
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: IBActions
+    @IBAction func didTogglePluto(_ sender: UISwitch) {
+        
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Planets/Views/PlanetCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PlanetCollectionViewCell.swift
+//  Planets
+//
+//  Created by Lambda_School_Loaner_259 on 2/19/20.
+//  Copyright © 2020 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class PlanetCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: IBOutlets
+    @IBOutlet var planetImageView: UIImageView!
+    @IBOutlet var planetNameLabel: UILabel!
+    
+    
+    
+}
